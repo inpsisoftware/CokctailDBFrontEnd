8ee73393d3e65f64cca9a333baf356c5689662d5
Sol Martinez <sol.inpsiweb@gmail.com>
2022-11-21 13:00:15 +0000

    cambiar direccion

--- programa.js
+++ programa.js
@@ -3,7 +3,7 @@ document.getElementById('botonBuscar').addEventListener('click',buscarDatos);
 function buscarDatos(){
     let Cokctail = document.getElementById('botonBuscar').value;
 
-    fetch("http://www.cocktailDBclone.somee.com/api/cocktail/buscar")
+    fetch("https://www.cocktailDBclone.somee.com/api/cocktail/buscar")
 .then((response => response.json()))
 .then((data)=> console.log(data));
 }
